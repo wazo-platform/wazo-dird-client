b7e79aa90c7c85eece298325fa58c15b1814c3d3
Etienne Lessard <elessard@avencall.com>
2014-11-27 13:51:41 -0500

    rename resource_url to base_url

--- xivo_dird_client/commands/directories.py
+++ xivo_dird_client/commands/directories.py
@@ -23,7 +23,7 @@ class DirectoriesCommand(BaseHTTPCommand):
     resource = 'directories'
 
     def lookup(self, profile, **kwargs):
-        url = '{base_url}/lookup/{profile}'.format(base_url=self.resource_url,
+        url = '{base_url}/lookup/{profile}'.format(base_url=self.base_url,
                                                    profile=profile)
         r = self.session.get(url, params=kwargs)
 
@@ -33,7 +33,7 @@ class DirectoriesCommand(BaseHTTPCommand):
         return r.json()
 
     def headers(self, profile, **kwargs):
-        url = '{base_url}/lookup/{profile}/headers'.format(base_url=self.resource_url,
+        url = '{base_url}/lookup/{profile}/headers'.format(base_url=self.base_url,
                                                            profile=profile)
 
         r = self.session.get(url, params=kwargs)
