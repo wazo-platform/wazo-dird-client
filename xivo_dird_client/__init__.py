13dc12c4512691b161f41a38c8d75be0deff6273
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-02-17 12:13:02 -0500

    Set a default timeout on all dird clients
    this timeout can be overridden by the Client configuration. This is only
    a sane default that should be acceptable in most cases.

--- xivo_dird_client/__init__.py
+++ xivo_dird_client/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2014 Avencall
+# Copyright (C) 2014-2015 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,4 +17,4 @@
 
 from xivo_lib_rest_client import make_client
 
-Client = make_client('dird_client.commands')
+Client = make_client('dird_client.commands', timeout=3)
