3f4e8b36efb95caafe1710cb45dd831725a0d242
Sebastien Duthil <sduthil@avencall.com>
2015-09-21 10:17:38 -0400

    verify_certificate defaults to False
    This was the behavior before refactoring. It will be changed later.

--- xivo_dird_client/client.py
+++ xivo_dird_client/client.py
@@ -22,5 +22,14 @@ class DirdClient(BaseClient):
 
     namespace = 'dird_client.commands'
 
-    def __init__(self, host, port=9489, version='0.1', **kwargs):
-        super(DirdClient, self).__init__(host=host, port=port, version=version, **kwargs)
+    def __init__(self,
+                 host,
+                 port=9489,
+                 version='0.1',
+                 verify_certificate=False,
+                 **kwargs):
+        super(DirdClient, self).__init__(host=host,
+                                         port=port,
+                                         version=version,
+                                         verify_certificate=verify_certificate,
+                                         **kwargs)
