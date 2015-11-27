9276c36861369cb5d5d2e560a3904186be7b38ae
François Blackburn <fblackburn@avencall.com>
2015-11-27 10:52:05 -0500

    remove reverse_me

--- xivo_dird_client/commands/directories.py
+++ xivo_dird_client/commands/directories.py
@@ -36,12 +36,6 @@ class DirectoriesCommand(RESTCommand):
         return r.json()
 
     def reverse(self, profile, xivo_user_uuid, token=None, **kwargs):
-        return self._reverse(profile, xivo_user_uuid, token, **kwargs)
-
-    def reverse_me(self, profile, token=None, **kwargs):
-        return self._reverse(profile, 'me', token, **kwargs)
-
-    def _reverse(self, profile, xivo_user_uuid, token=None, **kwargs):
         url = '{base_url}/reverse/{profile}/{xivo_user_uuid}'.format(base_url=self.base_url,
                                                                      profile=profile,
                                                                      xivo_user_uuid=xivo_user_uuid)
--- xivo_dird_client/commands/tests/test_directories.py
+++ xivo_dird_client/commands/tests/test_directories.py
@@ -61,22 +61,6 @@ class TestDirectories(RESTCommandTestCase):
 
         self.assertRaisesHTTPError(self.command.reverse, profile='my_profile', xivo_user_uuid='abcd-1234')
 
-    def test_reverse_me(self):
-        self.session.get.return_value = self.new_response(200, json={'return': 'value'})
-
-        result = self.command.reverse_me(profile='default', exten='1234', token=s.token)
-
-        self.session.get.assert_called_once_with(
-            '{base_url}/reverse/default/me'.format(base_url=self.base_url),
-            params={'exten': '1234'},
-            headers={'X-Auth-Token': s.token})
-        assert_that(result, equal_to({'return': 'value'}))
-
-    def test_reverse_me_when_not_200(self):
-        self.session.get.return_value = self.new_response(404)
-
-        self.assertRaisesHTTPError(self.command.reverse_me, profile='my_profile')
-
     def test_headers(self):
         self.session.get.return_value = self.new_response(200, json={'return': 'value'})
 
