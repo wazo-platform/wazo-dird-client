54a2c290040efab5b70869064697663562b12fbf
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-08-22 11:37:42 -0400

    phonebook: add phonebook contact api

--- xivo_dird_client/commands/phonebook.py
+++ xivo_dird_client/commands/phonebook.py
@@ -35,9 +35,31 @@ class PhonebookCommand(RESTCommand):
 
         return r.json()
 
+    def create_contact(self, token=None, tenant=None, phonebook_id=None, contact_body=None, **kwargs):
+        url = self._contact_all_url(tenant, phonebook_id)
+        headers = self._new_headers(token, content_type='application/json')
+
+        r = self.session.post(url, data=json.dumps(contact_body), params=kwargs, headers=headers)
+
+        if r.status_code != 201:
+            self.raise_from_response(r)
+
+        return r.json()
+
     def list(self, token=None, tenant=None, **kwargs):
         url = self._phonebook_all_url(tenant)
         headers = self._new_headers(token)
+        r = self.session.get(url, params=kwargs, headers=headers)
+
+        if r.status_code != 200:
+            self.raise_from_response(r)
+
+        return r.json()
+
+    def list_contacts(self, token=None, tenant=None, phonebook_id=None, **kwargs):
+        url = self._contact_all_url(tenant, phonebook_id)
+        headers = self._new_headers(token)
+
         r = self.session.get(url, params=kwargs, headers=headers)
 
         if r.status_code != 200:
@@ -73,14 +95,49 @@ class PhonebookCommand(RESTCommand):
 
         return r.json()
 
+    def get_contact(self, token=None, tenant=None, phonebook_id=None, contact_uuid=None, **kwargs):
+        url = self._contact_one_url(tenant, phonebook_id, contact_uuid)
+        headers = self._new_headers(token)
+
+        r = self.session.get(url, params=kwargs, headers=headers)
+
+        if r.status_code != 200:
+            self.raise_from_response(r)
+
+        return r.json()
+
+    def edit_contact(self, token=None, tenant=None, phonebook_id=None,
+                     contact_uuid=None, contact_body=None, **kwargs):
+        url = self._contact_one_url(tenant, phonebook_id, contact_uuid)
+        headers = self._new_headers(token, content_type='application/json')
+
+        r = self.session.put(url, data=json.dumps(contact_body), params=kwargs, headers=headers)
+
+        if r.status_code != 200:
+            self.raise_from_response(r)
+
+        return r.json()
+
+    def delete_contact(self, token=None, tenant=None, phonebook_id=None, contact_uuid=None, **kwargs):
+        url = self._contact_one_url(tenant, phonebook_id, contact_uuid)
+        headers = self._new_headers(token)
+
+        r = self.session.delete(url, params=kwargs, headers=headers)
+        if r.status_code != 204:
+            self.raise_from_response(r)
+
+    def _contact_all_url(self, tenant, phonebook_id):
+        return '{}/{}'.format(self._phonebook_one_url(tenant, phonebook_id), 'contacts')
+
+    def _contact_one_url(self, tenant, phonebook_id, contact_uuid):
+        return '{}/{}'.format(self._contact_all_url(tenant, phonebook_id), contact_uuid)
+
     def _phonebook_all_url(self, tenant):
         return '{base_url}/{tenant}/phonebooks'.format(base_url=self.base_url,
                                                        tenant=tenant)
 
     def _phonebook_one_url(self, tenant, phonebook_id):
-        return '{base_url}/{tenant}/phonebooks/{phonebook_id}'.format(base_url=self.base_url,
-                                                                      tenant=tenant,
-                                                                      phonebook_id=phonebook_id)
+        return '{}/{}'.format(self._phonebook_all_url(tenant), phonebook_id)
 
     @staticmethod
     def _new_headers(token, content_type=None):
--- xivo_dird_client/commands/tests/test_phonebook.py
+++ xivo_dird_client/commands/tests/test_phonebook.py
@@ -25,6 +25,141 @@ from mock import sentinel as s
 from ..phonebook import PhonebookCommand
 
 
+class TestPhonebookContact(RESTCommandTestCase):
+
+    Command = PhonebookCommand
+
+    def setUp(self):
+        super(TestPhonebookContact, self).setUp()
+        self.tenant = 'atenant'
+        self.phonebook_id = 42
+
+    def test_create(self):
+        self.session.post.return_value = self.new_response(201, json={'return': 'value'})
+        contact_body = {'firstname': 'Foo', 'lastname': 'Bar'}
+
+        result = self.command.create_contact(tenant=self.tenant,
+                                             phonebook_id=self.phonebook_id,
+                                             token=s.token, contact_body=contact_body)
+
+        url = '{base_url}/{tenant}/phonebooks/{phonebook_id}/contacts'.format(base_url=self.base_url,
+                                                                              tenant=self.tenant,
+                                                                              phonebook_id=self.phonebook_id)
+        self.session.post.assert_called_once_with(
+            url,
+            data=json.dumps(contact_body),
+            params={},
+            headers={'X-Auth-Token': s.token,
+                     'Content-Type': 'application/json'})
+        assert_that(result, equal_to({'return': 'value'}))
+
+    def test_create_when_not_201(self):
+        self.session.post.return_value = self.new_response(401)
+
+        self.assertRaisesHTTPError(self.command.create,
+                                   phonebook_body={},
+                                   tenant=self.tenant,
+                                   token=s.token)
+
+    def test_list(self):
+        self.session.get.return_value = self.new_response(200, json={'return': 'value'})
+
+        result = self.command.list_contacts(tenant=self.tenant,
+                                            token=s.token,
+                                            phonebook_id=self.phonebook_id)
+
+        url = '{base_url}/{tenant}/phonebooks/{phonebook_id}/contacts'.format(base_url=self.base_url,
+                                                                              tenant=self.tenant,
+                                                                              phonebook_id=self.phonebook_id)
+        self.session.get.assert_called_once_with(url, params={}, headers={'X-Auth-Token': s.token})
+        assert_that(result, equal_to({'return': 'value'}))
+
+    def test_list_when_not_200(self):
+        self.session.get.return_value = self.new_response(401)
+
+        self.assertRaisesHTTPError(self.command.list_contacts, token=s.token,
+                                   tenant=self.tenant, phonebook_id=self.phonebook_id)
+
+    def test_get(self):
+        self.session.get.return_value = self.new_response(200, json={'return': 'value'})
+
+        result = self.command.get_contact(token=s.token,
+                                          tenant=self.tenant,
+                                          phonebook_id=self.phonebook_id,
+                                          contact_uuid=s.contact_uuid)
+
+        url = '{base_url}/{tenant}/phonebooks/{phonebook_id}/contacts/{contact_uuid}'
+        url = url.format(base_url=self.base_url,
+                         tenant=self.tenant,
+                         phonebook_id=self.phonebook_id,
+                         contact_uuid=s.contact_uuid)
+        self.session.get.assert_called_once_with(url, params={}, headers={'X-Auth-Token': s.token})
+        assert_that(result, equal_to({'return': 'value'}))
+
+    def test_get_when_not_200(self):
+        self.session.get.return_value = self.new_response(401)
+
+        self.assertRaisesHTTPError(self.command.get_contact,
+                                   token=s.token,
+                                   tenant=self.tenant,
+                                   phonebook_id=self.phonebook_id,
+                                   contact_uuid=s.contact_uuid)
+
+    def test_edit(self):
+        self.session.put.return_value = self.new_response(200, json={'return': 'value'})
+        body = {'firstname': 'test'}
+
+        result = self.command.edit_contact(tenant=self.tenant,
+                                           phonebook_id=self.phonebook_id,
+                                           contact_uuid=s.contact_uuid,
+                                           contact_body=body,
+                                           token=s.token)
+
+        url = '{base_url}/{tenant}/phonebooks/{phonebook_id}/contacts/{contact_uuid}'
+        url = url.format(base_url=self.base_url,
+                         tenant=self.tenant,
+                         phonebook_id=self.phonebook_id,
+                         contact_uuid=s.contact_uuid)
+
+        self.session.put.assert_called_once_with(
+            url,
+            data=json.dumps(body),
+            params={},
+            headers={'X-Auth-Token': s.token,
+                     'Content-Type': 'application/json'})
+        assert_that(result, equal_to({'return': 'value'}))
+
+    def test_edit_when_not_200(self):
+        self.session.put.return_value = self.new_response(401)
+
+        self.assertRaisesHTTPError(self.command.edit_contact)
+
+    def test_delete(self):
+        self.session.delete.return_value = self.new_response(204)
+
+        result = self.command.delete_contact(tenant=self.tenant,
+                                             phonebook_id=self.phonebook_id,
+                                             contact_uuid=s.contact_uuid,
+                                             token=s.token)
+
+        url = '{base_url}/{tenant}/phonebooks/{phonebook_id}/contacts/{contact_uuid}'
+        url = url.format(base_url=self.base_url,
+                         tenant=self.tenant,
+                         phonebook_id=self.phonebook_id,
+                         contact_uuid=s.contact_uuid)
+        self.session.delete.assert_called_once_with(
+            url,
+            params={},
+            headers={'X-Auth-Token': s.token})
+        assert_that(result, none())
+
+    def test_delete_when_not_204(self):
+        self.session.delete.return_value = self.new_response(401)
+
+        self.assertRaisesHTTPError(self.command.delete, token=s.token, tenant=s.tenant,
+                                   phonebook_id=s.phonebook_id)
+
+
 class TestPhonebook(RESTCommandTestCase):
 
     Command = PhonebookCommand
