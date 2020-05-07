be0890aa80ac8adc7e850ebd524f158816c25837
Francois Blackburn <fblackburn@wazo.community>
2020-05-07 15:32:20 -0400

    update default config to use /api/dird on 443

--- wazo_dird_client/client.py
+++ wazo_dird_client/client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_lib_rest_client.client import BaseClient
@@ -9,5 +9,5 @@ class DirdClient(BaseClient):
 
     namespace = 'wazo_dird_client.commands'
 
-    def __init__(self, host, port=9489, version='0.1', **kwargs):
-        super(DirdClient, self).__init__(host=host, port=port, version=version, **kwargs)
+    def __init__(self, host, port=443, prefix='/api/dird', version='0.1', **kwargs):
+        super(DirdClient, self).__init__(host=host, port=port, prefix=prefix, version=version, **kwargs)
