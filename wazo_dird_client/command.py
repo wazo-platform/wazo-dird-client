38a23b32e6af16cf836320d43b8f02369807d353
nissou91 <achohra@wazo.io>
2022-05-19 09:16:07 -0400

    copyright headers: remove start year

--- wazo_dird_client/command.py
+++ wazo_dird_client/command.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_lib_rest_client.command import RESTCommand
--- wazo_dird_client/exceptions.py
+++ wazo_dird_client/exceptions.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
 
 from requests import HTTPError
 
